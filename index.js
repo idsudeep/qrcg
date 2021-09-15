7c5052053501c465eeb13b82cad5a496792c7546
idsudeep <sudeep.numb@gmail.com>
2021-09-16 00:34:26 +0530

    Done h.

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 // Calling the packages that we need
 
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 3000;
 const bp = require("body-parser");
 const qr = require("qrcode");
-
-
-app.set('views', path.join(__dirname,'views'));
+  
 app.set("view engine", "ejs");
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
-app.use(express.static('./views'));
 
+// Simple routing to the index.ejs file
 app.get("/", (req, res) => {
-    
-    res.render("index");
+    const fcode = 'Code';
+
+    qr.toDataURL(fcode, (err,src)=>{
+        if (err) res.send("Error occured");
+        res.render("index" , {src});
+    });
    
 });
 
-
+// Blank input
+// Incase of blank in the index.ejs file, return error 
+// Error  - Empty Data!
 app.post("/generate", (req, res) => {
+    const url = req.body.url;
 
-    res.render("index");
+    if (url.length === 0) 
+    res.send("Empty Data!");
+   
+    qr.toDataURL(url, (err, src) => {
+        if (err) res.send("Error occured");
+
+     
+        res.render("index", { src });
+    });
 });
 
 // Setting up the port for listening requests
-app.listen(port, () => console.log("Server at 5000"));
+app.listen(port, () => console.log("Server at 3000"));
