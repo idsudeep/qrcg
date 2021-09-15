9d5fe7401b72db5b0c34cc08d81dc65428d02434
idsudeep <sudeep.numb@gmail.com>
2021-09-16 00:21:33 +0530

    Done g.

--- index.js
+++ index.js
@@ -7,32 +7,23 @@ const port = 3000;
 const bp = require("body-parser");
 const qr = require("qrcode");
 
-// Using the ejs (Embedded JavaScript templates) as our template engine
-// and call the body parser  - middleware for parsing bodies from URL
-//                           - middleware for parsing json objects
+
 app.set('views', path.join(__dirname,'views'));
 app.set("view engine", "ejs");
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 app.use(express.static('./views'));
-// Simple routing to the index.ejs file
+
 app.get("/", (req, res) => {
     
     res.render("index");
    
 });
 
-// Blank input
-// Incase of blank in the index.ejs file, return error 
-// Error  - Empty Data!
+
 app.post("/generate", (req, res) => {
-    const url = req.body.url;
 
-
-
-     
-        res.render("index");
-    });
+    res.render("index");
 });
 
 // Setting up the port for listening requests
