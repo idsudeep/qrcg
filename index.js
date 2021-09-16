781f2132b61f31b3a965284b94c44f0537a02244
idsudeep <sudeep.numb@gmail.com>
2021-09-16 13:06:12 +0530

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT =  process.env.PORT || 3000;
 const bp = require("body-parser");
 const qr = require("qrcode");
   
@@ -39,4 +39,4 @@ app.post("/generate", (req, res) => {
 });
 
 // Setting up the port for listening requests
-app.listen(port, () => console.log("Server at 3000"));
+app.listen(PORT, () => console.log("Server at 3000"));
