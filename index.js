2a5204cc826b0d6ff45084a361e3f39ca1e25283
idsudeep <sudeep.numb@gmail.com>
2021-11-23 16:45:17 +0530

    https cs

--- index.js
+++ index.js
@@ -102,8 +102,41 @@ app.get("/removeUser",(req,res)=>{
     }
     removeUser().catch(console.dir);
     });
+app.get("/qrcg",(req,res)=>{
+    
+      res.setHeader("Access-Control-Allow-Origin", "*");
+     
+     
+    const url = req.query.url;
+    // console.log(url +'This Console');
+
+    
+   
+    qr.toDataURL(url, (err, src) => {
+        if (err) res.send("Error occured");
+
+        res.json({src});
+       
+      
+    });
 
 
+    
+});
+
+app.post("/generate", (req, res) => {
+    const url = req.body.url;
+    console.log(url +'This Console');
+
+    if (url.length === 0) 
+    res.send("Empty Data!");
+   
+    qr.toDataURL(url, (err, src) => {
+        if (err) res.send("Error occured");
+
+        res.render("index", { src });
+    });
+});
 
 app.get("/getQrcodec",(req,res)=>{
 
